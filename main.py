71736e01dc9642ffb97c9a1439e222479dbe32c1
HUD4K <mh.michalhudak@gmail.com>
2025-05-05 11:37:40 +0200

    refactoring
    ddfewewef

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask,  redirect, url_for,request
+# Je NUTNÉ nainštalovať alíček: do konzoly napíšte "pip install flask"
+from flask import Flask, request, render_template
 import sqlite3
 import hashlib
 
@@ -10,15 +11,12 @@ app = Flask(__name__)
 # <p> ...text... </p>                           - paragraf (normálny text)
 # <a href="www.---.com"> ...text...></a>        - odkaz (v rámci textu)
 # <button> ...text... </button>                 - tlačidlo s textom
-
+# <hr>                                          - čiara
 
 # Pripojenie k databáze
 def pripoj_db():
-    try:
-        conn = sqlite3.connect("kurzy.db")
-        return conn
-    except sqlite3.Error:
-        return None  # Ak sa nepodarí pripojiť, vráti None
+    conn = sqlite3.connect("kurzy.db")
+    return conn
 
 
 @app.route('/')  # API endpoint
@@ -26,49 +24,49 @@ def index():
     # Úvodná homepage s dvoma tlačidami ako ODKAZMI na svoje stránky - volanie API nedpointu
     return '''
         <h1>Výber z databázy</h1>
-        <ul class="navbar">
-        <li><a href="/registracia">1 - Registrácia nového trénera</a></li>
-        <li><a href="/treneri_a_kurz">2 - Výpis všetkých trénerov a ich kurzov</a></li>
-        <li><a href="/kurzy">3 - Výpis všetkých kurzov</a></li>
-        <li><a href="/miesta">4 - Výpis všetkých miest </a></li>
-        <li><a href="/capacita">5 - Výpis súčtu maximálnej kapacity všetkých kurzov na P</a></li>
-        </ul>
-        
-
-        <style>
-        .navbar{
-        list-style-type: none;
-        
-
-        }
-        h1{
-        font-family: "Afacad Flux", sans-serif;
-        font-weight:700;
-        font-size:200px;
-        }
-        .navbar li{
-        margin: 0 5 8 0;
-        display: inline;
-        float: left;
-        padding:15px;
-        background-color: #04AA6D;
-        }
-        a{
-        text-decoration:none;
-        display: block;
-        padding: 5px;
-        color:black;
-        font-family: "Afacad Flux", sans-serif;
-        font-weight:600;
-        }
-        li:hover {
-            background-color:rgb(2, 118, 75);
-        }
-
-        </style>
+        <a href="/kurzy"><button>Zobraz všetky kurzy</button></a>
+        <a href="/treneri"><button>Zobraz všetkých trénerov</button></a>
+        <a href="/registracia"><button>Registruj trénera</button></a>
+        <hr>
     '''
 
 
+@app.route('/kurzy')
+def zobraz_kurzy():
+    conn = pripoj_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM Kurzy")
+    kurzy = cursor.fetchall()
+    conn.close()
+
+    return render_template("kurzy.html", kurzy=kurzy)
+
+
+# PODSTRÁNKA NA ZOBRAZENIE TRÉNEROV
+@app.route('/treneri')  # API endpoint
+def zobraz_trenerov():
+    conn = pripoj_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT T.ID_trenera, T.Meno || ' ' || T.Priezvisko as Trener, Nazov_kurzu
+        FROM Treneri T LEFT JOIN Kurzy K ON T.ID_trenera = K.ID_trenera
+    """)
+    treneri = cursor.fetchall()
+
+    conn.close()
+
+    # Jednoduchý textový výpis trénerov a ich kurzov
+    vystup = "<h2>Zoznam trénerov a kurzov:</h2>"
+    for trener in treneri:
+        vystup += f"<p>{trener}</p>"
+
+    # Odkaz na návrat
+    vystup += '<a href="/">Späť</a>'
+    return vystup
+
+# Metóda je GET. (Predtým sme metódu nedefinovali. Ak žiadnu neuvedieme, automaticky je aj tak GET)
 @app.route('/registracia', methods=['GET'])
 def registracia_form():
     return '''
@@ -123,345 +121,6 @@ def registracia_trenera():
         <hr>
         <a href="/">Späť</a>
     '''
-
-
-
-@app.route('/kurzy')  
-def zobraz_kurzy():
-    conn = pripoj_db()
-    if conn is None:
-        return redirect(url_for('zobraz_error'))  # Ak DB nefunguje, presmeruje na 404
-    
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM Kurzy")
-    kurzy = cursor.fetchall()
-
-    conn.close()
-
-    # Hlavička tabuľky (prispôsob podľa štruktúry tabuľky v DB)
-    vystup = """
-    <h2>Zoznam kurzov:</h2>
-    <table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Názov kurzu</th>
-            
-        </tr>
-
-
-    <style>
-     table {
-        width: 80%;
-        border-collapse: collapse;
-        margin: 20px 0;
-        font-size: 18px;
-        text-align: left;
-        font-family: "Afacad Flux", sans-serif;
-        font-weight:450;
-    }
-    th, td {
-        padding: 12px;
-        border: 1px solid black;
-        
-    }
-    th {
-        background-color:  #04AA6D;
-    }
-
-    button {
-        border: none;
-        background-color: white;
-        color:black;
-        padding: 16px 32px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        margin: 4px 2px;
-        transition-duration: 0.4s;
-        cursor: pointer;
-        border: 2px solid #04AA6D;
-        font-family: "Afacad Flux", sans-serif;
-        font-weight:600;
-        
-}
-
-
-button:hover {
-  background-color: #04AA6D;
-  color: white;
-}
-</style>
-    """
-
-    # Naplnenie tabuľky dátami
-    for kurz in kurzy:
-        vystup += f"<tr><td>{kurz[0]}</td><td>{kurz[1]}</td></tr>"
-
-    vystup += "</table>"  # Uzavrieme tabuľku
-    vystup += '<br><a href="/"><button>Späť</button></a>'  # Tlačidlo na návrat
-    return vystup
-
-
-
-
-@app.route('/miesta')  
-def zobraz_miesta():
-    conn = pripoj_db()
-    if conn is None:
-        return redirect(url_for('zobraz_error'))  # Ak DB nefunguje, presmeruje na 404
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM Miesta")
-    miesta = cursor.fetchall()
-
-    conn.close()
-
-    vystup = """
-    <h2>Zoznam miest:</h2>
-    <table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Názov miesta</th>
-            <th>Adresa</th>
-        </tr>
-
-         <style>
-    table {
-        width: 80%;
-        border-collapse: collapse;
-        margin: 20px 0;
-        font-size: 18px;
-        text-align: left;
-        font-family: "Afacad Flux", sans-serif;
-        font-weight:450;
-    }
-    th, td {
-        padding: 12px;
-        border: 1px solid black;
-    }
-    th {
-        background-color:  #04AA6D;
-    }
-
-    button {
-        border: none;
-        background-color: white;
-        color:black;
-        padding: 16px 32px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        margin: 4px 2px;
-        transition-duration: 0.4s;
-        cursor: pointer;
-        border: 2px solid #04AA6D;
-        font-family: "Afacad Flux", sans-serif;
-        font-weight:600;
-}
-
-
-button:hover {
-  background-color: #04AA6D;
-  color: white;
-  font-family: "Afacad Flux", sans-serif;
-  font-weight:600;
-}
-</style>
-    """
-
-    for miesto in miesta:
-        vystup += f"<tr><td>{miesto[0]}</td><td>{miesto[1]}</td><td>{miesto[2]}</td></tr>"
-
-    vystup += "</table>"
-    vystup += '<br><a href="/"><button>Späť</button></a>'
-    return vystup
-
-
-
-# PODSTRÁNKA NA ZOBRAZENIE TRÉNEROV
-@app.route('/treneri_a_kurz')  # API endpoint
-def zobraz_trenerov_kurz():
-    conn = pripoj_db()
-    if conn is None:
-        return redirect(url_for('zobraz_error'))  # Ak DB nefunguje, presmeruje na 404
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM VSETCI_TRENERI_A_ICH_KURZY")
-    kurzy = cursor.fetchall()
-
-    conn.close()
-
-    # Hlavička tabuľky (prispôsob podľa štruktúry tabuľky v DB)
-    vystup = """
-    <h2>Zoznam kurzov:</h2>
-    <table border="1">
-        <tr>
-            <th>ID Trenera</th>
-            <th>Meno Trenera</th>
-            <th>Názov kurzu</th>
-        </tr>
-
-
-         <style>
-    table {
-        width: 80%;
-        border-collapse: collapse;
-        margin: 20px 0;
-        font-size: 18px;
-        text-align: left;
-        font-family: "Afacad Flux", sans-serif;
-        font-weight:450;
-    }
-    th, td {
-        padding: 12px;
-        border: 1px solid black;
-    }
-    th {
-        background-color:  #04AA6D;
-    }
-
-    button {
-        border: none;
-        background-color: white;
-        color:black;
-        padding: 16px 32px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        margin: 4px 2px;
-        transition-duration: 0.4s;
-        cursor: pointer;
-        border: 2px solid #04AA6D;
-        font-family: "Afacad Flux", sans-serif;
-        font-weight:600;
-}
-
-
-button:hover {
-  background-color: #04AA6D;
-  color: white;
-  font-family: "Afacad Flux", sans-serif;
-  font-weight:600;
-}
-</style>
-    """
-
-    # Naplnenie tabuľky dátami
-    for trener in kurzy:
-        vystup += f"<tr><td>{trener[0]}</td><td>{trener[1]}</td><td>{trener[2]}</td></tr>"
-
-    vystup += "</table>"  # Uzavrieme tabuľku
-    vystup += '<br><a href="/"><button>Späť</button></a>'  # Tlačidlo na návrat
-    return vystup
-
-
-@app.route('/capacita')  # API endpoint
-def zobraz_capacitu():
-    conn = pripoj_db()
-    if conn is None:
-        return redirect(url_for('zobraz_error'))  # Ak DB nefunguje, presmeruje na 404
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT SUM(Max_pocet_ucastnikov) FROM Kurzy WHERE Nazov_kurzu LIKE 'P%'")
-    capacita = cursor.fetchall()
-
-    conn.close()
-
-    # Hlavička tabuľky (prispôsob podľa štruktúry tabuľky v DB)
-    vystup = """
-    <h2>Zoznam kurzov:</h2>
-    <table border="1">
-        <tr>
-            <th>Suma</th>
-            
-        </tr>
-
-
-        <style>
-    table {
-        width: 80%;
-        border-collapse: collapse;
-        margin: 20px 0;
-        font-size: 18px;
-        text-align: left;
-        font-family: "Afacad Flux", sans-serif;
-        font-weight:450;
-    }
-    th, td {
-        padding: 12px;
-        border: 1px solid black;
-    }
-    th {
-        background-color:  #04AA6D;
-    }
-
-    button {
-        border: none;
-        background-color: white;
-        color:black;
-        padding: 16px 32px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        margin: 4px 2px;
-        transition-duration: 0.4s;
-        cursor: pointer;
-        border: 2px solid #04AA6D;
-        font-family: "Afacad Flux", sans-serif;
-        font-weight:600;
-}
-
-
-button:hover {
-  background-color: #04AA6D;
-  color: white;
-}
-</style>
-    """
-
-    # Naplnenie tabuľky dátami
-    for cap in capacita:
-        vystup += f"<tr><td>{cap[0]}</td></tr>"
-
-    vystup += "</table>"  # Uzavrieme tabuľku
-    vystup += '<br><a href="/"><button>Späť</button></a>'  # Tlačidlo na návrat
-    return vystup
-
-
-@app.route('/404')
-def zobraz_error():
-    return '''
-    <h1>ERROR 404 :(</h1>
-    <p>Požadovaná stránka neexistuje alebo sa vyskytla chyba pri načítaní databázy.</p>
-    <br><a href="/"><button>Späť</button></a>
-    <style>
-        h1 { color: red; }
-        button {
-            border: none;
-            background-color: white;
-            color: black;
-            padding: 16px 32px;
-            font-size: 16px;
-            border: 2px solid #04AA6D;
-            cursor: pointer;
-            font-family: "Afacad Flux", sans-serif;
-            font-weight:450;
-        }
-        button:hover {
-            background-color: #04AA6D;
-            color: white;
-            font-family: "Afacad Flux", sans-serif;
-            font-weight:450;
-        }
-    </style>
-    '''
-    vystup += '<br><a href="/"><button>Späť</button></a>'  
-    return vystup
 
 
 if __name__ == '__main__':
